78694a6ea2e585a491ba3d130789ac6eb9eb554b
rodolfo <rodolfo@moda.(none)>
2013-11-08 17:54:51 -0300

    model vision

--- src/web/cafeeiro/model/model_empresa_ts_by_cnpj.php
+++ src/web/cafeeiro/model/model_empresa_ts_by_cnpj.php
@@ -2,7 +2,7 @@
     include 'global_model.php';
 
     # Read the GET parameters
-    $cnpj = $_GET['cnpj'];
+    $scenario = $_GET['scenario'];
 
     # Connect to the Database
     $conn = odbc_connect($dsn,'','') or die ("CONNECTION ERROR\n");
@@ -12,8 +12,24 @@
     # ----------------------------------------------------------------------------------------
 
     # Prepare the query
-    // $cnpj = '86550951000150';
-    $query = str_replace("[EMP_CNPJ]", $cnpj, $query_map['get_isin_with_largest_ts_by_cnpj']);
+    if ($scenario == 'Varginha_alta_tx5'){
+        $query = $query_map['get_scenario_varginha_alta_tx5']
+    }
+        if ($scenario == 'Varginha_baixa_tx5'){
+        $query = $query_map['get_scenario_varginha_baixa_tx5']
+    }
+        if ($scenario == 'Varginha_alta_tx10'){
+        $query = $query_map['get_scenario_varginha_alta_tx10']
+    }
+        if ($scenario == 'Tudo_alta_tx5'){
+        $query = $query_map['get_scenario_tudo_alta_tx5']
+    }
+        if ($scenario == 'Tudo_baixa_tx5'){
+        $query = $query_map['get_scenario_tudo_baixa_tx5']
+    }
+        if ($scenario == 'Tudo_baixa_tx10'){
+        $query = $query_map['get_scenario_tudo_baixa_tx10']
+    }
 
     # Execute the query
     $resultset = odbc_prepare($conn, $query);
@@ -24,33 +40,8 @@
         array_push($isin_row, $row);
     }
     
-    if ($isin_row[0]['tamanho_cotacao'] <= 0){
-        # There is no acao to all ISINs of that CNPJ, so we return the empty time-serie.
-        echo json_encode(array());
-    }else{
-        # ----------------------------------------------------------------------------------------
-        # QUERY 2: Get the acao by isin
-        # ----------------------------------------------------------------------------------------
-        $selected_isin = $isin_row[0]['cod_isin'];
-        
-        # Prepare the query
-        $query = str_replace("[EMP_ISIN]", $selected_isin, $query_map['get_ts_by_isin_with_solavanco']);
-       
-        # Execute the query
-        $resultset = odbc_prepare($conn, $query);
-        $success = odbc_execute($resultset, array());
-
-        # Fetch all rows
-        $list_response = array();
-        while ($row = odbc_fetch_array($resultset)) {
-            array_push($list_response, array(strtotime($row['data_pregao']) * 1000, 
-                                            (float) $row['preco_ultimo'], 
-                                            $row['is_solavanco']));
-        }
-
-        echo json_encode(array($list_response, $selected_isin));
-    }
-
+    echo json_encode(array($list_response, $selected_isin));
+    
     # Close the connection
     odbc_close($conn);
 ?>
---
+++ src/web/cafeeiro/model/model_scenario_ts_by_scenario.php
@@ -0,0 +1,53 @@
+<?php 
+    include 'global_model.php';
+
+    # Read the GET parameters
+    $scenario = $_GET['scenario'];
+
+    # Connect to the Database
+    $conn = odbc_connect($dsn,'','') or die ("CONNECTION ERROR\n");
+
+    # ----------------------------------------------------------------------------------------
+    # QUERY 1: Get the scenarios
+    # ----------------------------------------------------------------------------------------
+
+    # Prepare the query
+    if ($scenario == 'Varginha_alta_tx5'){
+        $query = $query_map['get_scenario_varginha_alta_tx5'];
+    }
+
+    if ($scenario == 'Varginha_baixa_tx5'){
+        $query = $query_map['get_scenario_varginha_baixa_tx5'];
+    }
+
+    if ($scenario == 'Varginha_alta_tx10'){
+        $query = $query_map['get_scenario_varginha_alta_tx10'];
+    }
+
+    if ($scenario == 'Tudo_alta_tx5'){
+        $query = $query_map['get_scenario_tudo_alta_tx5'];
+    }
+
+    if ($scenario == 'Tudo_baixa_tx5'){
+        $query = $query_map['get_scenario_tudo_baixa_tx5'];
+    }
+
+    if ($scenario == 'Tudo_baixa_tx10'){
+        $query = $query_map['get_scenario_tudo_baixa_tx10'];
+    }
+
+    # Execute the query
+    $resultset = odbc_prepare($conn, $query);
+    $success = odbc_execute($resultset, array());
+
+    $incidencia_rows = array();
+    while ($row = odbc_fetch_array($resultset)) {
+        array_push($incidencia_rows, $row);
+    }
+    
+    echo json_encode(array($incidencia_rows));
+    
+    # Close the connection
+    odbc_close($conn);
+?>
+
